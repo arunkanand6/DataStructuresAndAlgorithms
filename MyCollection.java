025be2deeab17ef9b66cdb96f0cb1203c121398a
Arun Kumar M <67538899+arunkanand6@users.noreply.github.com>
2020-09-13 10:36:14 +0530

    Create MyCollection.java

---
+++ MyCollection.java
@@ -0,0 +1,12 @@
+public interface MyCollection<T> {
+	
+	//Returns the size of the list
+	public int size();
+		
+	//Returns true if the list is empty else false
+	public boolean isEmpty();
+		
+	//Removes all the elements from the list
+	public boolean clear();
+  
+}
