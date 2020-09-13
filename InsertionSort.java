b20c4442406fdebd5af0071a15775660d80ddbe3
Arun Kumar M <67538899+arunkanand6@users.noreply.github.com>
2020-09-13 10:51:29 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,38 @@
+/* Insertion Sort:---
+ * --------------------
+ * Stable Sorting Algorithm
+ * Comparison Sort
+ * Complexity:-
+ * +----------------+---------+
+ * |Best Case		    |	O(N)		|
+ * |----------------|---------|
+ * |Worst Case		  |	O(N^2)	|
+ * |----------------|---------|
+ * |Average Case	  |	O(N^2)	|
+ * |----------------|---------|
+ * |Space			      |	O(1)		|
+ * +----------------+---------+
+ */
+
+public class InsertionSort<T> {
+	
+	public T[] insertionSort(T[] arr) {
+		if(!((arr[0] instanceof Number)|(arr[0] instanceof Character))) {
+			System.out.println("The given arr is not sortable");
+			return null;
+		}
+		int i, j;
+		T temp, key;
+		for(i=1; i<arr.length; i++) {
+			key = arr[i];
+			j=i-1;
+			while(j>=0 && ((Number)key).doubleValue()<((Number)arr[j]).doubleValue()) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				j--;
+			}
+		}
+		return arr;
+	}
+}
